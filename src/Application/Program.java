6d748dd5aeac30c728d827cbc8a8ad918ae660d2
Thiago <batista.thiago2002@gmail.com>
2024-10-29 10:49:33 -0300

    Initial commit

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,2 +1,42 @@
-package Application;public class Program {
+package Application;
+
+import Entities.Task;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        List<Task> todolist = new ArrayList<>();
+
+        String taskName;
+        String option;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        do {
+            System.out.println("Enter a task: ");
+            taskName = input.nextLine();
+
+            System.out.println("Enter the date and time for the task (dd/MM/yyyy HH:mm): ");
+            String taskDate = input.nextLine();
+            LocalDateTime taskDateTime = LocalDateTime.parse(taskDate, formatter);
+
+            todolist.add(new Task(taskName, taskDateTime));
+
+            System.out.println("Task added successfully! Do you want to add any other tasks? (yes/no): ");
+            option = input.nextLine();
+        } while (option.equalsIgnoreCase("yes"));
+
+        System.out.println("Your To-Do List:");
+        for (Task task : todolist) {
+            System.out.println(task);
+        }
+
+        input.close();
+    }
 }
--- src/Entities/Task.java
+++ src/Entities/Task.java
@@ -1,2 +1,28 @@
-package Entities;public class Task {
+package Entities;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Task {
+    private String task;
+    private LocalDateTime dateTime;
+
+    public Task(String task, LocalDateTime dateTime) {
+        this.task = task;
+        this.dateTime = dateTime;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        return "Task: " + task + "| Date and Time: " + dateTime.format(formatter);
+    }
 }
